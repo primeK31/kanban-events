4cce209146874bd8dbfa0bc998193fada41486a3
primeK31 <ashirbekov005@gmail.com>
2024-06-11 23:12:14 +0500

    app

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/lecture1');
+        await mongoose.connect('mongodb+srv://admin:<tipa_password>@cluster0.imtaoos.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
         console.log('MongoDB connected...');
     } catch (err:any) {
         console.error(err.message);
